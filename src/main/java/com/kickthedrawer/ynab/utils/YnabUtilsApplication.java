cf7708e61fc137821d3c70b46f26adca86a81045
Tom Warne <twarne@gmail.com>
2023-01-27 18:02:29 -0700

    Spring initializer bootstrap

---
+++ src/main/java/com/kickthedrawer/ynab/utils/YnabUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.kickthedrawer.ynab.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnabUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnabUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kickthedrawer/ynab/utils/YnabUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickthedrawer.ynab.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnabUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
